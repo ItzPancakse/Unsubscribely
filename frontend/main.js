b11e006663d7e1db8b77e8ab79f1af1e4e365a08
Pancakse <84547061+ItzPancakse@users.noreply.github.com>
2026-09-19 16:09:17 -0600

    i give up on wails

--- frontend/main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('test-button').addEventListener('click', async () => {
-    const msg = await window.go.main.App.Greet();
-    document.getElementById('response').textContent  = msg;
-});
